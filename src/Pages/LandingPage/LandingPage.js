516a1ddf95115adcd7c040087059dce74da31456
aamohamedo <aamohamedo@icloud.com>
2023-06-12 14:28:15 +0100

    created skeleton for landing page

--- src/Pages/LandingPage/LandingPage.js
+++ src/Pages/LandingPage/LandingPage.js
@@ -1,14 +1,22 @@
 import { Link } from "react-router-dom";
+import "./LandingPage.css";
+// import MaterialUI dependencies
+import { Button } from "@mui/material";
 
 function LandingPage() {
-    return <>
-            <button>
-                <Link to="/src/pages/createpostform">Create Post Form</Link>
-            </button>
-            <button>
-                <Link to="/src/pages/signsignup">Sign In / Sign Up Page</Link>
-            </button>
-        </>
+  return (
+    <div id="landing-page-outer-container">
+      <Button variant="contained">
+        <Link to="/src/pages/carddisplay">Find a clean up</Link>
+      </Button>
+      <Button variant="contained">
+        <Link to="/src/pages/signsignup">Create a clean up</Link>
+      </Button>
+      <div id="landing-page-banner">
+        <h1>Unite, Transform and Clean Up your community.</h1>
+      </div>
+    </div>
+  );
 }
 
 export default LandingPage;
