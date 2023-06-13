eec114f6ddf332678f192170c2a729cbda86d248
Felicity Price <fpricework@gmail.com>
2023-06-13 15:35:56 +0100

    Started to fix desktop card display css.

--- src/Pages/CardDisplay/CardDisplay.js
+++ src/Pages/CardDisplay/CardDisplay.js
@@ -1,4 +1,6 @@
 // import { Link } from "react-router-dom";
+import './CardDisplay.css';
+
 import React from "react";
 import Card from "./components/Card/Card.js";
 import SearchAppBar from "../Navbar/Navbar.js";
@@ -7,41 +9,43 @@ function CardDisplay() {
   return (
     <div>
       <SearchAppBar />
-      <Card
-        header="Let's clean up Pentewan beach!"
-        color="#FC786E"
-        body={`Hi guys!
-  You've likely noticed the horrific state of our beloved beach as of late!
-  Let's do something about it! If I can get some of you to assist me, I can provide gloves and bin bags!`}
-      />
-            <Card
-        header="Let's clean up Pentewan beach!"
-        color="#55DADB"
-        body={`Hi guys!
-  You've likely noticed the horrific state of our beloved beach as of late!
-  Let's do something about it! If I can get some of you to assist me, I can provide gloves and bin bags!`}
-      />
-            <Card
-        header="Let's clean up Pentewan beach!"
-        color="#6AAF88"
-        body={`Hi guys!
-  You've likely noticed the horrific state of our beloved beach as of late!
-  Let's do something about it! If I can get some of you to assist me, I can provide gloves and bin bags!`}
-      />
-            <Card
-        header="Let's clean up Pentewan beach!"
-        color="#F5BB02"
-        body={`Hi guys!
-  You've likely noticed the horrific state of our beloved beach as of late!
-  Let's do something about it! If I can get some of you to assist me, I can provide gloves and bin bags!`}
-      />
-            <Card
-        header="Let's clean up Pentewan beach!"
-        color="#FC786E"
-        body={`Hi guys!
-  You've likely noticed the horrific state of our beloved beach as of late!
-  Let's do something about it! If I can get some of you to assist me, I can provide gloves and bin bags!`}
-      />
+      <div id="card-display-outer-container">
+        <Card
+          header="Let's clean up Pentewan beach!"
+          color="#FC786E"
+          body={`Hi guys!
+    You've likely noticed the horrific state of our beloved beach as of late!
+    Let's do something about it! If I can get some of you to assist me, I can provide gloves and bin bags!`}
+        />
+              <Card
+          header="Let's clean up Pentewan beach!"
+          color="#55DADB"
+          body={`Hi guys!
+    You've likely noticed the horrific state of our beloved beach as of late!
+    Let's do something about it! If I can get some of you to assist me, I can provide gloves and bin bags!`}
+        />
+              <Card
+          header="Let's clean up Pentewan beach!"
+          color="#6AAF88"
+          body={`Hi guys!
+    You've likely noticed the horrific state of our beloved beach as of late!
+    Let's do something about it! If I can get some of you to assist me, I can provide gloves and bin bags!`}
+        />
+              <Card
+          header="Let's clean up Pentewan beach!"
+          color="#F5BB02"
+          body={`Hi guys!
+    You've likely noticed the horrific state of our beloved beach as of late!
+    Let's do something about it! If I can get some of you to assist me, I can provide gloves and bin bags!`}
+        />
+              <Card
+          header="Let's clean up Pentewan beach!"
+          color="#FC786E"
+          body={`Hi guys!
+    You've likely noticed the horrific state of our beloved beach as of late!
+    Let's do something about it! If I can get some of you to assist me, I can provide gloves and bin bags!`}
+        />
+      </div>
     </div>
   );
 }
