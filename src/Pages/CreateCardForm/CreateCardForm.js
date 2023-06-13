5d49864f3db902048bb4b4a9f3412cc080702d77
trisrosco <trisrosco@gmail.com>
2023-06-13 20:52:03 +0100

    Had to make a janky theme in order to change the highlight colour, lord forgive me

--- src/Pages/CreateCardForm/CreateCardForm.js
+++ src/Pages/CreateCardForm/CreateCardForm.js
@@ -2,45 +2,46 @@ import React from "react";
 import Navbar from "../Navbar/Navbar";
 import "./CreateCardForm.css";
 import { Stack, Typography, TextField } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+// This was the only way I could change the colour of the text field highlight
+// const jankTheme = createTheme({
+//   palette: {
+//     primary: {
+//       main: "#55DADB" 
+//     }
+//   }
+// });
 
 export default function CreateCardForm() {
   return (
-    <div id="create-card-outer-container">
-      <Navbar />
-      <Typography variant="h3" id="create-card-title">
-        Create a Post
-      </Typography>
-      <Stack spacing={2} direction="column" id="create-card-form-container">
-        <TextField
-          //   required
-          id="post-title"
-          label="Title"
-          defaultValue=""
-          variant="filled"
-          sx={{
-            "& .MuiFilledInput-input:focus": {
-              backgroundColor: "#D9D9D9",
-              color: "black",
-            },
-          }}
-        />
-        <TextField
-          //   required
-          id="location-address"
-          label="Location address"
-          defaultValue=""
-          variant="filled"
-        />
-        <TextField
-          //   required
-          id="location-postcode"
-          label="Location Postcode"
-          defaultValue=""
-          variant="filled"
-        />
-        
-        
-      </Stack>
-    </div>
+    // <ThemeProvider theme={jankTheme}> 
+      <div id="create-card-outer-container">
+        <Navbar />
+        <Typography variant="h3" id="create-card-title">
+          Create a Post
+        </Typography>
+        <Stack spacing={2} direction="column" id="create-card-form-container">
+          <TextField
+            id="post-title"
+            label="Title"
+            defaultValue=""
+            variant="filled"
+          />
+          <TextField
+            id="location-address"
+            label="Location address"
+            defaultValue=""
+            variant="filled"
+          />
+          <TextField
+            id="location-postcode"
+            label="Location Postcode"
+            defaultValue=""
+            variant="filled"
+          />
+        </Stack>
+      </div>
+    // </ThemeProvider>
   );
 }
