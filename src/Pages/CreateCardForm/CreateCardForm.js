4bc4f9fab922f40c6ed0bd70c4be4a23498898e0
trisrosco <trisrosco@gmail.com>
2023-06-13 18:34:09 +0100

    Realised that I'd been creating a user signup page. Inputs re-labled now

--- src/Pages/CreateCardForm/CreateCardForm.js
+++ src/Pages/CreateCardForm/CreateCardForm.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
-import { Link } from "react-router-dom";
 import "./CreateCardForm.css";
 import { Stack, Typography, TextField } from "@mui/material";
 
 export default function CreateCardForm() {
   return (
     <div id="create-card-outer-container">
-    
       <Navbar />
       <Typography variant="h3" id="create-card-title">
         Create a Post
@@ -15,21 +13,34 @@ export default function CreateCardForm() {
       <Stack spacing={2} direction="column" id="create-card-form-container">
         <TextField
           //   required
-          id="first-name"
-          label="First name"
+          id="post-title"
+          label="Title"
           defaultValue=""
           variant="filled"
-          sx={{ "& .MuiFilledInput-input:focus": { backgroundColor: "#D9D9D9", color: "black"} }}
+          sx={{
+            "& .MuiFilledInput-input:focus": {
+              backgroundColor: "#D9D9D9",
+              color: "black",
+            },
+          }}
         />
         <TextField
           //   required
-          id="second-name"
-          label="Surname"
+          id="location-address"
+          label="Location address"
           defaultValue=""
           variant="filled"
         />
+        <TextField
+          //   required
+          id="location-postcode"
+          label="Location Postcode"
+          defaultValue=""
+          variant="filled"
+        />
+        
+        
       </Stack>
-
     </div>
   );
 }
