aa34c2a34698617dd3021df12b76e7b1bbaad290
Felicity Price <fpricework@gmail.com>
2023-06-14 16:28:52 +0100

    Moved Navbar file import reference.

--- src/Pages/CreateCardForm/CreateCardForm.js
+++ src/Pages/CreateCardForm/CreateCardForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Navbar from "../Navbar/Navbar";
+import Navbar from "../Components/Navbar/Navbar";
 import "./CreateCardForm.css";
 import {
   Stack,
