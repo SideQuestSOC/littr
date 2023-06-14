8479a8a66cf73ae705eb604e227a3683168e2477
Felicity Price <fpricework@gmail.com>
2023-06-14 13:23:57 +0100

    Added link to landing page onto the Littr logo/text and stripped default style off of the <a> tag.

--- src/Pages/Components/Navbar/Navbar.js
+++ src/Pages/Components/Navbar/Navbar.js
@@ -108,7 +108,7 @@ export default function SearchAppBar() {
               marginLeft: "10px",
             }}
           >
-          LITTR
+            <Link to="/">LITTR</Link>
           </Typography>
 
           <IconButton
