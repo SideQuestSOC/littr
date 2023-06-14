5022cfc2e9b4b0b57d25c9b8b745ed3f6a0a0d12
trisrosco <trisrosco@gmail.com>
2023-06-14 14:30:47 +0100

    Styling (mostly) finished!

--- src/Pages/CreateCardForm/CreateCardForm.js
+++ src/Pages/CreateCardForm/CreateCardForm.js
@@ -13,21 +13,21 @@ import {
   FormControl,
   InputLabel,
   Select,
-  MenuItem
+  MenuItem,
 } from "@mui/material";
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { MultiInputTimeRangeField } from '@mui/x-date-pickers-pro/MultiInputTimeRangeField';
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { MultiInputTimeRangeField } from "@mui/x-date-pickers-pro/MultiInputTimeRangeField";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 // This was the only way I could change the colour of the text field highlight
 const jankTheme = createTheme({
   palette: {
     primary: {
-      main: "#6AAF88"
-    }
-  }
+      main: "#6AAF88",
+    },
+  },
 });
 
 export default function CreateCardForm() {
@@ -67,63 +67,23 @@ export default function CreateCardForm() {
           {/* Date picker below */}
           <LocalizationProvider dateAdapter={AdapterDayjs}>
             <DatePicker
-              label="Please Select a Date"
-              className="date-picker" // Add className to style the date picker
+              label="Date of event"
+              className="date-picker"
+              format="DD/MM/YYYY"
+              variant="inline"
             />
             <MultiInputTimeRangeField
+              id="time-range"
               slotProps={{
                 textField: ({ position }) => ({
-                  label: position === 'start' ? 'From' : 'To',
-                  className: 'time-range-field' 
+                  label: position === "start" ? "Start time" : "End time",
+                  className: "time-range-field",
                 }),
               }}
             />
           </LocalizationProvider>
-          {/* Somewhere here we can add a date/time picker or some individual ones */}
-          <Divider />
-          <Typography id="accessability-title" variant="h6">
-            Accessibility information
-          </Typography>
-          {/* Accessibility checkboxes */}
-          <FormGroup id="accessibility-checkboxes">
-            <FormControlLabel control={<Checkbox />} label="Nearby Bathrooms" />
-            <FormControlLabel control={<Checkbox />} label="Uneven ground" />
-            <FormControlLabel control={<Checkbox />} label="Remote location" />
-            <FormControlLabel
-              //  I added a defaultChecked prop to the checkbox to show how it works
-              control={<Checkbox defaultChecked />}
-              label="Nearby Parking"
-            />
-          </FormGroup>
-          <Divider />
-          <Typography id="recommended-equipment-title" variant="h8">
-            Recommended equipment
-          </Typography>
-          {/* The below text field does not like being styled */}
-          <TextField
-            id="recommended-equipment"
-            multiline
-            rows={3}
-            variant="standard"
-          />
-          <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">Disposal Method</InputLabel>
-            <Select
-              labelId="-select-label"
-              id="demo-simple-select"
-              value={age}
-              label="method"
-              onChange={handleChange}
-              className="dropdown" // Add className to style the dropdown
-            >
-              <MenuItem value={1}>Pickers must dispose of their own litter</MenuItem>
-              <MenuItem value={2}>Council pick-up</MenuItem>
-              <MenuItem value={3}>On-site Refuse disposal</MenuItem>
-              <MenuItem value={4}>Literal dumpster fire</MenuItem>
-            </Select>
-          </FormControl>
           <Typography id="additional-information-title" variant="h8">
-            Additional information
+            Details
           </Typography>
           <TextField
             id="additional-information"
@@ -131,7 +91,64 @@ export default function CreateCardForm() {
             rows={3}
             variant="standard"
           />
+          <Divider />
+          <Typography id="accessability-title" variant="h6">
+            Accessibility information
+          </Typography>
+          {/* Accessibility checkboxes */}
+          <FormGroup id="accessibility-checkboxes">
+            <FormControlLabel
+              control={<Checkbox />}
+              className="checkbox"
+              label="Nearby Bathrooms"
+            />
+            <FormControlLabel
+              control={<Checkbox />}
+              className="checkbox"
+              label="Uneven ground"
+            />
+            <FormControlLabel
+              control={<Checkbox />}
+              className="checkbox"
+              label="Remote location"
+            />
+            <FormControlLabel
+              //  I added a defaultChecked prop to the checkbox to show how it works
+              control={<Checkbox defaultChecked />}
+              className="checkbox"
+              label="Nearby Parking"
+            />
+          </FormGroup>
+          <Divider />
+          {/* Disposal method */}
+          <FormControl fullWidth>
+            <InputLabel id="disposal-method">Disposal Method</InputLabel>
+            <Select
+              labelId="-select-label"
+              id="disposal-select"
+              value={age}
+              label="method"
+              onChange={handleChange}
+            >
+              <MenuItem value={1}>
+                Pickers must dispose of their own litter
+              </MenuItem>
+              <MenuItem value={2}>Council pick-up</MenuItem>
+              <MenuItem value={3}>On-site Refuse disposal</MenuItem>
+              <MenuItem value={4}>Literal dumpster fire</MenuItem>
+            </Select>
+          </FormControl>
+          <Typography id="recommended-equipment-title" variant="h8">
+            Recommended equipment
+          </Typography>
+          <TextField
+            id="recommended-equipment"
+            multiline
+            rows={3}
+            variant="standard"
+          />
 
+          {/* Buttons */}
           <Stack spacing={2} direction="row" id="create-card-button-container">
             <Button id="discard-button" variant="contained">
               Discard
