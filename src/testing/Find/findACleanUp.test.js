01ac0d6851a0ee58aae59ae45fd193eb049c0fbd
stevenlegere <stevenmarklegere@gmail.com>
2023-06-16 11:02:54 +0100

    Working on unit test for CardDisplay

--- src/testing/Find/findACleanUp.test.js
+++ src/testing/Find/findACleanUp.test.js
@@ -1,6 +1,9 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom';
 import LandingPage from '../../Pages/LandingPage/LandingPage';
+import App from '../../Pages/App/App'
 
 // Check if find a clean up button renders
 test("renders find a clean up button", () => {
@@ -19,3 +22,16 @@ test("renders find a clean up button", () => {
 
 // Above test passes
 
+// Check if the button can be pressed by simulating a click event and navigates to card display page
+
+test('button click should navigate to CardDisplay page', () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>
+    );
+  
+    fireEvent.click(screen.getByText('Find a clean up'));
+  
+    expect(screen.getByTestId('card-display')).toBeInTheDocument();
+  });
