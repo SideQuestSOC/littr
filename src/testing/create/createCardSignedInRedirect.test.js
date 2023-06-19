5a244a6b136603b6efd7f0d290b2a72c2436da2c
stevenlegere <stevenmarklegere@gmail.com>
2023-06-19 22:10:26 +0100

    Added successful test to test for user signed in or signed out when navigating to CreateCardForm. If signed in then render, if signed out then redirect to sign in page

--- src/testing/create/createCardSignedInRedirect.test.js
+++ src/testing/create/createCardSignedInRedirect.test.js
@@ -1,7 +1,35 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { Router } from 'react-router-dom';
-import CreateCardForm from '../../Pages/CreateCardForm/CreateCardForm';
+import { render, waitFor } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import CreateCardForm from '../../Pages/CreateCardForm/CreateCardForm.js';
 
-// Write test that check if user is signed import
+const mockNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockNavigate,
+}));
 
+describe('CreateCardForm', () => {
+  it('should redirect to card display page when user is not signed in', async () => {
+    // Arrange
+    const isSignedIn = false;
+    const setIsSignedIn = jest.fn();
+    const setCardData = jest.fn();
+
+    // Act
+    render(
+      <MemoryRouter>
+        <CreateCardForm
+          isSignedIn={isSignedIn}
+          setIsSignedIn={setIsSignedIn}
+          setCardData={setCardData}
+        />
+      </MemoryRouter>
+    );
+
+    // Assert
+    await waitFor(() => {
+      expect(mockNavigate).toHaveBeenCalledWith('/src/pages/carddisplay');
+    });
+  });
+});
