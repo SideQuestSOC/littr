4127bb3bb0b40fb5ba9324cf3aec9bca9ee3a4ac
Josh Simpson <uk.j.simpson@gmail.com>
2023-06-20 10:43:06 +0100

    footer2

--- src/Pages/Components/Footer/footer.js
+++ src/Pages/Components/Footer/footer.js
@@ -1,16 +1,16 @@
 import React from "react";
 import FacebookIcon from '@mui/icons-material/Facebook';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import "./Footer.css";
 
 const Footer = () => {
     return (
-        <footer>
-            <div className="footer">
+
+            <footer className="footer">
                 <FacebookIcon />
                 <LinkedInIcon />
-            </div>
-        </footer>
-    );
+            </footer> 
+           );
 };
 
 export default Footer;
