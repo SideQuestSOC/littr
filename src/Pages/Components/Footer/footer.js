ef12caffe4c893a8cdfae692b815a7302301dc20
Josh Simpson <uk.j.simpson@gmail.com>
2023-06-20 11:17:15 +0100

    footerGitadded

--- src/Pages/Components/Footer/footer.js
+++ src/Pages/Components/Footer/footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FacebookIcon from '@mui/icons-material/Facebook';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import "./Footer.css";
 
 const Footer = () => {
@@ -8,6 +9,7 @@ const Footer = () => {
             <footer className="footer">
               <a href="https://www.facebook.com/"target= "_blank" rel="noopener noreferrer"><FacebookIcon className="icon" /></a>
               <a href="https://www.linkedin.com/"target= "_blank" rel="noopener noreferrer"><LinkedInIcon className="icon" /></a>
+              <a href="https://github.com/"target= "_blank" rel="noopener noreferrer"><GitHubIcon className="icon" /></a>
             </footer> 
            );
 };
