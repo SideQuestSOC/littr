9c72b24d371afe236fb26cc56fa2f36df0bc0eb7
Josh Simpson <uk.j.simpson@gmail.com>
2023-06-21 14:13:21 +0100

    newest

--- src/Pages/CardDisplay/components/Like/ThumbsUp.js
+++ src/Pages/CardDisplay/components/Like/ThumbsUp.js
@@ -8,13 +8,13 @@ const ThumbsUp = ({ event_id, initialLikes }) => {
 
   const handleThumbsUp = async () => {
     try {
-      // Call the incrementLikes function to increment the likes
+    
       await incrementLikes(event_id);
 
-      // Update the likes state
+  
       setLikes(likes + 1);
     } catch (error) {
-      // Handle any errors
+
       console.log('Error incrementing likes:', error);
     }
   };
