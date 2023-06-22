c21b72fffe798c94e73748ad134393dd23f0810a
Felicity Price <fpricework@gmail.com>
2023-06-22 13:30:56 +0100

    Added handling for if a not signed in user clicks like button.

--- src/Pages/CardDisplay/components/LikeButton/LikeButton.js
+++ src/Pages/CardDisplay/components/LikeButton/LikeButton.js
@@ -6,16 +6,20 @@ import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt";
 import { updateLikes } from "../../../../Models/queries";
 
 export default function LikeButton(props) {
+  async function handleThumbsUp(event_id, isSignedIn) {
 
-  async function handleThumbsUp(event_id) {
-    await updateLikes(event_id);
-    props.setUpdateLikeBadge(true);
+    if(isSignedIn) {
+      await updateLikes(event_id);
+     props.setUpdateLikeBadge(true);
+    } else {
+      alert("Please Sign In to Like an Event!");
+    }
   }
 
   return (
     <Button
       id="like-button"
-      onClick={() => { handleThumbsUp(props.event_id) }}
+      onClick={() => { handleThumbsUp(props.event_id, props.isSignedIn) }}
       variant="contained"
       data-testid="like-button"
     >
