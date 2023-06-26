edae19fdb241b8d02966c801f2e0ba0ffb0f7c1a
Josh Simpson <uk.j.simpson@gmail.com>
2023-06-26 12:27:54 +0100

    likeVer2

--- src/Pages/CardDisplay/components/LikeButton/LikeButton.js
+++ src/Pages/CardDisplay/components/LikeButton/LikeButton.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from "react";
 import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt";
-import { updateLikes, countLikes } from "../../../../Models/queries";
+import { updateLikes, countLikes, checkIfLiked } from "../../../../Models/queries";
 import { getCurrentUserId } from "../../../../Models/client";
-import { checkIfLiked } from "../../../../Models/queries";
 import LikeBadge from "./LikeBadge";
 
 const LikeButton = ({ event_id, isSignedIn }) => {
   const [user_id, setUserID] = useState("");
-  const [isLiked, setIsLiked] = useState(0);
+  const [isLiked, setIsLiked] = useState(false);
   const [likesCount, setLikesCount] = useState(0);
 
   useEffect(() => {
@@ -19,17 +18,16 @@ const LikeButton = ({ event_id, isSignedIn }) => {
     }
   }, [isSignedIn]);
 
-  // Check if the current user has already liked this event
   useEffect(() => {
-    if (isSignedIn) {
-      async function fetchLikedStatus() {
-        setIsLiked(await checkIfLiked(event_id));
+    async function fetchLikedStatus() {
+      if (isSignedIn) {
+        const liked = await checkIfLiked(event_id);
+        setIsLiked(liked);
       }
-      fetchLikedStatus();
     }
+    fetchLikedStatus();
   }, [event_id, isSignedIn]);
 
-  // Fetch the likes count for the event
   useEffect(() => {
     async function fetchLikesCount() {
       const count = await countLikes(event_id);
@@ -39,17 +37,20 @@ const LikeButton = ({ event_id, isSignedIn }) => {
   }, [event_id]);
 
   const handleUpdateLikes = async () => {
-    await updateLikes(user_id, event_id);
-    setLikesCount(likesCount + 1);
+    if (!isLiked && isSignedIn) {
+      await updateLikes(user_id, event_id);
+      setLikesCount(likesCount + 1);
+      setIsLiked(true);
+    }
   };
 
   return (
     <div>
       <ThumbUpOffAltIcon
         id="like-button"
-        onClick={isSignedIn ? handleUpdateLikes : () => alert("Please Sign In to Like!")}
+        onClick={handleUpdateLikes}
+        disabled={isLiked || !isSignedIn}
       />
-      {/* Render the LikeBadge component with the likesCount */}
       <LikeBadge count={likesCount} />
     </div>
   );
