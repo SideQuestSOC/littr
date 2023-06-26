ab5a8787fc99336242a57fc401b3601a56d37036
Diana <dpoldver@gmail.com>
2023-06-26 20:59:57 +0100

    the test for adding Recommended Equipment is complete and passes

---
+++ src/testing/create/createRecommendedEquipment.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import CreateCardForm from '../../Pages/CreateCardForm/CreateCardForm';
+
+test('user can add recommended equipment', () => {
+  // Render the CreateCardForm component within a Router
+  render(
+    <Router>
+      <CreateCardForm isSignedIn={true} />
+    </Router>
+  );
+
+  // Type recommended equipment in the input field
+  const recommendedEquipmentInput = screen.getByPlaceholderText("e.g. gloves, pickers, water");
+
+  fireEvent.change(recommendedEquipmentInput, {
+    target: { value: 'gloves, pickers, water' },
+  });
+
+  // Assert that the input value is set correctly
+  expect(recommendedEquipmentInput.value).toBe('gloves, pickers, water');
+});
