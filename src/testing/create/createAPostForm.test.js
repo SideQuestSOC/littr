0bff1aa21b3415976362a41b489dec3e22cfe3fa
stevenlegere <stevenmarklegere@gmail.com>
2023-06-26 08:03:17 +0100

    Test pass for createAPostForm.test.js redirect when signed out and then renders when signed in

--- src/testing/create/createAPostForm.test.js
+++ src/testing/create/createAPostForm.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import { MemoryRouter, Routes, Route } from 'react-router-dom';
-import userEvent from '@testing-library/user-event';
 import SignInSignUp from '../../Pages/SignInSignUp/SignInSignUp.js';
 import CreateCardForm from '../../Pages/CreateCardForm/CreateCardForm.js';
+import { supabase } from '../../Models/client.js';
+
 
 // Test that a signed-in user can access the create a post form
 describe('CreateCardForm component test all elements', () => {
@@ -23,65 +24,20 @@ describe('CreateCardForm component test all elements', () => {
       expect(screen.getByTestId('sign-in-sign-up-page')).toBeInTheDocument();
     });
   });
+
+// Test that a signed-in user can access the create a post form
+  test("render the CreateCardForm component if signed in", () => {
+  render(
+    <MemoryRouter>
+      <CreateCardForm
+        isSignedIn={true}
+        setIsSignedIn={() => {}}
+        setCardData={() => {}}
+        setFilter={() => {}}
+      />
+    </MemoryRouter>
+  );
+  const createCardFormElement = screen.getByTestId("create-card-form");
+  expect(createCardFormElement).toBeInTheDocument();
 });
-
-
-
-
-
-// import React from "react";
-// import { render, screen } from "@testing-library/react";
-// import { MemoryRouter, Route, Routes, useNavigate } from "react-router-dom";
-// import CreateCardForm from "../../Pages/CreateCardForm/CreateCardForm.js";
-// import SignInSignUp from "../../Pages/SignInSignUp/SignInSignUp.js";
-
-
-
-
-// // Test that a signed in user can access the create a post form
-
-// describe("CreateCardForm component test all elements", () => {
-//   it("render the CreateCardForm component", () => {
-//     render(
-//       <MemoryRouter>
-//         <CreateCardForm
-//           isSignedIn={true}
-//           setIsSignedIn={() => {}}
-//           setCardData={() => {}}
-//           setFilter={() => {}}
-//         />
-//       </MemoryRouter>
-//     );
-//     const createCardFormElement = screen.getByTestId("create-card-form");
-//     expect(createCardFormElement).toBeInTheDocument();
-//   });
-
-// //   // Test that a signed out user cannot access the create a post form and is redirected to signinSignUp page
-//   it("redirects to SignInSignUp page if user is signed out", () => {
-//     render(
-//       <MemoryRouter
-//         initialEntries={["/src/pages/CardDisplay/CardDisplay.js"]}
-//         initialIndex={0}
-//       >
-//         <Routes>
-//           <Route
-//             path="/src/pages/CardDisplay/CardDisplay.js"
-//             element={
-//               <CreateCardForm
-//                 isSignedIn={false}
-//                 setIsSignedIn={() => {}}
-//                 setCardData={() => {}}
-//                 setFilter={() => {}}
-//               />
-//             }
-//           />
-//           <Route path="/src/pages/SignInSignUp/SignInSignUp.js" element={<SignInSignUp />} />
-//         </Routes>
-//       </MemoryRouter>
-//     );
-
-//     // Check if the SignInSignUp page is rendered
-//     const heading = screen.getByText("Sign In");
-//     expect(heading).toBeInTheDocument();
-//   });
-// });
+});
