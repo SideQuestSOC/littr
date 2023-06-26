720984ab7ee158554ae8e2fcaabee03f0da5e12b
stevenlegere <stevenmarklegere@gmail.com>
2023-06-26 11:11:25 +0100

    createAPostForm.test.js Successful title of post

--- src/testing/create/createAPostForm.test.js
+++ src/testing/create/createAPostForm.test.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import { MemoryRouter, Routes, Route } from 'react-router-dom';
 import SignInSignUp from '../../Pages/SignInSignUp/SignInSignUp.js';
 import CreateCardForm from '../../Pages/CreateCardForm/CreateCardForm.js';
-import { supabase } from '../../Models/client.js';
-
 
 // Test that a signed-in user can access the create a post form
 describe('CreateCardForm component test all elements', () => {
@@ -40,4 +38,42 @@ describe('CreateCardForm component test all elements', () => {
   const createCardFormElement = screen.getByTestId("create-card-form");
   expect(createCardFormElement).toBeInTheDocument();
 });
+
+// Test that the Title of Post accepts user input with character limits
+test('User can enter a valid title of post', async () => {
+  const validTitle = 'Valid Title';
+
+  // Mock the window.alert function
+    const mockAlert = jest.spyOn(window, 'alert');
+    mockAlert.mockImplementation(() => {});
+
+  render(
+    <MemoryRouter>
+      <CreateCardForm
+      isSignedIn={true}
+      setIsSignedIn={() => {}}
+      setCardData={() => {}}
+      setFilter={() => {}}
+      />
+    </MemoryRouter>
+  );
+
+  const titleInput = screen.getByTestId('post-title-input');
+  const submitButton = screen.getByText('Create Post');
+
+titleInput.value = validTitle;
+fireEvent.change(titleInput);
+
+  expect(titleInput.value).toBe(validTitle);
+
+  fireEvent.click(submitButton);
+
+  // Assert that the form is submitted successfully
+  await waitFor(() => {
+    expect(screen.queryByLabelText('Title must be at least 5 characters')).toBeNull();
+  });
+  // Restore the original window.alert function
+  mockAlert.mockRestore();
+});
+
 });
