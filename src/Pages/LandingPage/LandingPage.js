86c32400e540b242f1fb1bef5e6422225209c338
Felicity Price <fpricework@gmail.com>
2023-06-27 12:21:39 +0100

    Removed redirect from landing page.

--- src/Pages/LandingPage/LandingPage.js
+++ src/Pages/LandingPage/LandingPage.js
@@ -1,21 +1,10 @@
 import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom"; // useNavigate() is used to redirect to a different page
-import { useEffect } from "react";
 import "./LandingPage.css";
 // import MaterialUI dependencies
 import { Button } from "@mui/material";
 import Footer from "../Components/Footer/footer";
 
 function LandingPage({ isSignedIn, setFilter }) {
-  // Initialize the navigate object using the useNavigate 'hook'
-  const navigate = useNavigate();
-
-  // Redirect to Card Display Page if already logged in
-  useEffect(() => {
-    if(isSignedIn) {
-      navigate("/src/pages/carddisplay");
-    }
-  }, [isSignedIn, navigate]);
 
   return <>
     <div id="landing-page-outer-container">
