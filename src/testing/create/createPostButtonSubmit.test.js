a40afd9810ddee359b8d75d0786e5807b155e955
stevenlegere <stevenmarklegere@gmail.com>
2023-07-03 09:33:35 +0100

    createPostButtonSubmit.test.js now with postcode check

--- src/testing/create/createPostButtonSubmit.test.js
+++ src/testing/create/createPostButtonSubmit.test.js
@@ -13,4 +13,36 @@ describe('Create Post Button to submit form', () => {
 
         expect(createPostButton).toBeInTheDocument();
 });
+
+    test('should display an alert when required fields are missing', () => {
+        const alertMock = jest.spyOn(window, 'alert').mockImplementation(() => {
+            render(
+                <Router>
+                <CreateCardForm />
+                </Router>
+            );
+            fireEvent.click(screen.getByTestId('create-post-button-submit'));
+            expect(alertMock).toHaveBeenCalledWith('Please fill it all in!');
+            alertMock.mockRestore();
+        });
+        });
+
+    test('should display an alert when the postcode is invalid', () => {
+        // Render the component
+        const alertMock = jest.spyOn(window, 'alert').mockImplementation(() => {
+            render(
+                <Router>
+                <CreateCardForm />
+                </Router>
+            );
+            // Set an invalid postcode
+            fireEvent.change(screen.getByLabelText('Postcode'), { target: { value: '1234' }});
+
+            // Trigger the submit button
+            fireEvent.click(screen.getByTestId('create-post-button-submit'));
+
+            expect(alertMock).toHaveBeenCalledWith('Please enter a valid postcode!');
+            alertMock.mockRestore();
+        })
+    })
 })
